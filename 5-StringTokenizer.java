296d6ad8d6049ba394d1d262974d35391d3f9339
Ojus Garg <ojusgarg52@gmail.com>
2026-07-27 12:29:47 +0530

    Add StringTokenizer example in 5-StringTokenizer.java

---
+++ 5-StringTokenizer.java
@@ -0,0 +1,15 @@
+import java.util.StringTokenizer;
+
+public class stringtokenizer {
+    
+    public static void main(String[] args) {
+        String str = "Hello, World! Welcome to Java programming.";
+        StringTokenizer st = new StringTokenizer(str, ",");
+
+        while (st.hasMoreTokens()) {
+            System.out.println(st.nextToken());
+
+        }
+
+    }
+}
